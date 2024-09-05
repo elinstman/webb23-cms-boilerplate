2ce054fb02d5c7b8a5aae82cd879170852965919
Elin Östman <elin.ostman@hotmail.com>
2024-09-05 13:57:39 +0200

    not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,14 @@
-export default function NotFound() {
-    //Fetch the 404 page from storyblok (this component works as server component aswell)
-    //update this component to render a 404 page
-    return <h1>404 notfound</h1>
+import { StoryblokCMS } from "@/utils/cms";
+
+export default async function NotFound() {
+  const currentConfig = await StoryblokCMS.getConfig();
+  console.log("404", currentConfig);
+  //Fetch the 404 page from storyblok (this component works as server component aswell)
+  //update this component to render a 404 page
+  return (
+    <div>
+      <h1>{currentConfig?.notFoundTitle} </h1>
+      <p> {currentConfig?.notFoundText} </p>
+    </div>
+  );
 }
